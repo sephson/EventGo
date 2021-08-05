95341798cee088b43d82ac1c4896b3a60772e2f2
sephson <disutj@gmail.com>
2021-08-05 11:24:14 +0100

    working on the create event page

--- frontend/src/pages/CreateEvent/CreateEvent.js
+++ frontend/src/pages/CreateEvent/CreateEvent.js
@@ -12,44 +12,53 @@ const CreateEvent = () => {
       </div>
 
       <form className="event-info">
-        <h1>Basic Info</h1>
-        <input className="event-info-input" placeholder="Event Title" />
-        <input className="event-info-input" placeholder="Organiser" />
+        <div className="basic-info">
+          <h1>Basic Info</h1>
+          <input className="event-info-input" placeholder="Event Title" />
+          <input className="event-info-input" placeholder="Organiser" />
+        </div>
         <h3 className="event-info-location">Location</h3>
-        <span>Venue</span>
-        <span>Online</span>
+        <span className="online-or-venue">Venue</span>
+        <span className="online-or-venue online">Online</span>
 
         <h3 className="event-info-location">Date and Time</h3>
         <p>
           Let the people who would attend your event when know when your event
           starts
         </p>
-        <span>One Day event</span>
-        <span>Multiple days event</span>
-        <input
-          className="event-info-input"
-          type="date"
-          placeholder="Event Date (starts)"
-        />
-        <input
-          className="event-info-input"
-          type="time"
-          placeholder="Event Time (starts)"
-        />
-        <input
-          className="event-info-input"
-          type="date"
-          placeholder="Event Date (ends)"
-        />
-        <input
-          className="event-info-input"
-          type="time"
-          placeholder="Event Time (end)"
-        />
+        <span className="online-or-venue">One Day event</span>
+        <span className="online-or-venue online">Multiple days event</span>
+        <div className="dandt">
+          <div className="starts one-day">
+            <input
+              className="event-info-input"
+              type="date"
+              placeholder="Event Date (starts)"
+            />
+            <input
+              className="event-info-input"
+              type="time"
+              placeholder="Event Time (starts)"
+            />
+          </div>
+          <div className="end">
+            <input
+              className="event-info-input"
+              type="date"
+              placeholder="Event Date (ends)"
+            />
+            <input
+              className="event-info-input"
+              type="time"
+              placeholder="Event Time (end)"
+            />
+          </div>
+        </div>
 
         <h1>Main Event</h1>
-        <input type="image" placeholder="Event Image" alt="event image" />
+        <input type="file" classname="img" name="img" accept="image/*" />
         <textarea className="event-info-input" placeholder="Description" />
+        <input type="number" placeholder="price" />
 
         <button>Publish </button>
       </form>
