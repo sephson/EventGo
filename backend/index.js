922500ac817fefa6f90510cdac2311f56e7c70d2
sephson <disutj@gmail.com>
2021-09-01 09:55:02 +0100

    -prep for first deployment

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,19 @@ app.use("/event", eventRoute);
 app.use("/upload", uploadRoute);
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
+  });
+}
+
 app.use(errorHandler);
 
 const server = app.listen(process.env.PORT, () =>
