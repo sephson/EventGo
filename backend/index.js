e7987f3477690443c2ac2eea2152c01f191b643b
sephson <disutj@gmail.com>
2021-09-06 13:16:37 +0100

    deploy

--- backend/index.js
+++ backend/index.js
@@ -24,17 +24,17 @@ app.use("/upload", uploadRoute);
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join(__dirname, '/client/build')))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
     res.send("Api running");
   });
 }
-
+//https://eventgo-app.herokuapp.com/
 app.use(errorHandler);
 
 const server = app.listen(process.env.PORT || 5000, () =>
